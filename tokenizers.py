1ed9aac0ae530b3681ee1c987f37d455cbb35ad0
Sean Fillingham <spfillingham@gmail.com>
2025-01-29 12:29:13 -0500

    added simple tokenizers from chapter 2

---
+++ tokenizers.py
@@ -0,0 +1,36 @@
+# A few simple tokenization methods that are part of chapter 2
+import re
+
+class SimpleTokenizerV1:
+    def __init__(self, vocab):
+        self.str_to_int = vocab
+        self.int_to_str = {i:s for s, i in vocab.items()}
+
+    def encode(self, text):
+        preprocessed = re.split(r'([,.?"()\']|--|\s)', text)
+        preprocessed = [item.strip() for item in preprocessed if item.strip()]
+        ids = [self.str_to_int[s] for s in preprocessed]
+        return ids
+    
+    def decode(self, ids):
+        text = ' '.join([self.int_to_str[i] for i in ids])
+        text = re.sub(r'\s+([,.?"()\'])', r'\1', text)
+        return text
+    
+class SimpleTokenizerV2:
+    def __init__(self, vocab):
+        self.str_to_int = vocab
+        self.int_to_str = {i:s for s, i in vocab.items()}
+
+    def encode(self, text):
+        preprocessed = re.split(r'([,.?"()\']|--|\s)', text)
+        preprocessed = [item.strip() for item in preprocessed if item.strip()]
+        preprocessed = [item for item in self.str_to_int else "<|unk|>" for item in preprocessed]
+
+        ids = [self.str_to_int[s] for s in preprocessed]
+        return ids
+    
+    def decode(self, ids):
+        text = ' '.join([self.int_to_str[i] for i in ids])
+        text = re.sub(r'\s+([,.?"()\'])', r'\1', text)
+        return text
