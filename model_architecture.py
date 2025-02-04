20961b17e7fb504088991d2832519161a2e7d133
Sean Fillingham <spfillingham@gmail.com>
2025-02-04 13:11:32 -0500

    Implement TransformerBlock class in model_architecture.py

--- model_architecture.py
+++ model_architecture.py
@@ -1,6 +1,8 @@
 import torch
 import torch.nn as nn
 
+from attention_heads import MultiHeadAttention
+
 
 class DummyGPTModel(nn.Module):
     def __init__(self, cfg):
@@ -82,3 +84,35 @@ class FeedForward(nn.Module):
 
     def forward(self, x):
         return self.layers(x)
+    
+
+class TransformerBlock(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.att = MultiHeadAttention(
+            d_in=cfg['emb_dim'],
+            d_out=cfg['emb_dim'],
+            context_length=cfg['context_length'],
+            num_heads=cfg['n_heads'],
+            dropout=cfg['drop_rate'],
+            qkv_bias=cfg['qkv_bias']
+        )
+        self.ff = FeedForward(cfg)
+        self.norm1 = LayerNorm(cfg)
+        self.norm2 = LayerNorm(cfg)
+        self.drop_shortcut = nn.Dropout(cfg['drop_rate'])
+
+    def forward(self, x):
+        shortcut = x
+        x = self.norm1(x)
+        x = self.att(x)
+        x = self.drop_shortcut(x)
+        x = x + shortcut
+
+        shortcut = x
+        x = self.norm2(x)
+        x = self.ff(x)
+        x = self.drop_shortcut(x)
+        x = x + shortcut
+
+        return x
